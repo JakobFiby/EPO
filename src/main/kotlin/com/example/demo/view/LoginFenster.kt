b27ae898de8cc32a45ea3330e9c4aa0a07acb7a4
kathimith <kathi.schramm.ks@gmail.com>
2020-10-24 12:50:49 +0200

    LoginFenster hbox, vbox, button rechts, padding

--- src/main/kotlin/com/example/demo/view/LoginFenster.kt
+++ src/main/kotlin/com/example/demo/view/LoginFenster.kt
@@ -2,35 +2,57 @@ package com.example.demo.view
 
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Orientation
+import javafx.geometry.Pos
+import javafx.scene.text.Font
+import javafx.scene.text.FontWeight
 import tornadofx.*
+import javax.swing.text.Position
 
 class LoginFenster : View("Login") {
     val model = ViewModel()
     val nutzername= model.bind{SimpleStringProperty()}
     val passwort= model.bind{SimpleStringProperty()}
 
-
     override val root = form {
+    hbox{
+        paddingTop=10
+        paddingBottom=10
+        paddingRight=20
+        paddingLeft=20
 
-        fieldset(title, labelPosition = Orientation.VERTICAL){
-            fieldset("Nutzername"){
-                textfield(nutzername).required()
+        vbox(spacing=20){
+            vbox(spacing=20) {
+            hbox{
+                label("Wir sind "){
+                font= Font.font("Segoe UI", FontWeight.LIGHT, 30.0)
             }
-            fieldset("Passwort"){
-                passwordfield(passwort).required()
-            }
-            button("Anmelden"){
-                enableWhen(model.valid)
-                isDefaultButton=true
-                useMaxHeight=true
-                action{
-                    runAsyncWithProgress {
-
-                    }
+                label("EPO"){
+                    font= Font.font("Segoe UI", FontWeight.BOLD, 30.0)
                 }
             }
+            }
+        fieldset(title, labelPosition = Orientation.VERTICAL) {
+            fieldset("Nutzername") {
+                textfield(nutzername).required()
+            }
+            fieldset("Passwort") {
+                passwordfield(passwort).required()
+            }
 
+            vbox(0,Pos.TOP_RIGHT){
+                button("Anmelden"){
+                    enableWhen(model.valid)
+                    isDefaultButton = true
+                    useMaxHeight = true
+                    action {
+                        runAsyncWithProgress {
+
+                        }
+                }
+            }
         }
-
-    }
+        }
+        }
+        }
+}
 }

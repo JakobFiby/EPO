eff49cec0c5a93ab716b39c83954ff5720f6da34
Jakob Fiby <Jakob.Fiby@s.hakmistelbach.ac.at>
2020-11-07 11:09:01 +0100

    xter Versuch den DB-Zugriff zu schaffen

--- src/main/kotlin/com/example/demo/view/Test.kt
+++ src/main/kotlin/com/example/demo/view/Test.kt
@@ -9,14 +9,18 @@ object Test {
 
     @JvmStatic
     fun connection(){
+        Class.forName("com.microsoft.sqlserver")
         try{
+            // @Kathi --> ich hab mir so einen jdbc Driver runtergeladen, aba keinen Plan, war das jz doch nd funkt
+            //i bin wirklich am verzweifeln
+
             val c = DriverManager.getConnection(
                     "jdbc:mysql://sql516.your-server.de:3306/epofgs",
                     "digbizm_7", "2021##Epo5ck"
             )
             println("OK")
             val s=c.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)
-            val r=s.executeQuery("SELECT `userid`, `nutzername`, `passwort`, `mitteilungen`, `abmelden`, `profilbild`, `vorname`, `nachname`, `email`, `farbschemaid` FROM `user` WHERE 1")
+            val r=s.executeQuery("SELECT * FROM user")
         }
         catch(e:SQLException){
             e.printStackTrace()
